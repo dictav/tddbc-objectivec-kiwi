40635b81ecbce365038e197622983633137cdb86
Shintaro Abe <dictav@gmail.com>
2013-05-12 10:38:10 +0900

    Initial Commit

---
+++ TDDBC_KiwiSample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TDDBC_KiwiSample
+//
+//  Created by Abe Shintaro on 2013/05/12.
+//  Copyright (c) 2013年 Abe Shintaro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
